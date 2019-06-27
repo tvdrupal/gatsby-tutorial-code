8ca1d7a165944399a16692f2074755bb6b9b918d
hijasonzhang <hereweare_521@163.com>
2019-06-27 14:52:25 +0800

    note...

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  siteMetadata: {
+    title: `Title from siteMetadata cccc`,
+  },
+  plugins: [
+   {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+  
+}
